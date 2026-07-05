d54e27d4cf32f848063fb1002ce2bf1f7cf49ada
uvesh_nagani <164164923+naganiuvesh@users.noreply.github.com>
2026-07-05 19:54:47 +0530

    strings in JavaScript

---
+++ JavaScript/01_basics/05_strings.js
@@ -0,0 +1,19 @@
+let name = "uvesh";
+const surname = "Nagani";
+console.log(surname);  
+console.log(name);  
+console.log(name.toUpperCase()); // it does't change original string 
+
+// ``(backticks)
+console.log(`My name is ${name} and my surname is ${surname}`);
+
+// string 
+const carName = new String("virtus");
+console.log(carName);
+console.log(carName.toUpperCase()); // it does't change original string 
+console.log(carName.charAt(3));
+console.log(carName.indexOf('t'));
+
+console.log(carName.trim());
+
+// all string methods on MDN Docs you can try on it.
