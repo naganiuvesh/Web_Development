c6997881c99381c72ab929d7930929183955b705
uvesh_nagani <164164923+naganiuvesh@users.noreply.github.com>
2026-07-05 19:17:43 +0530

    Add files via upload

---
+++ JavaScript/01_variables.js
@@ -0,0 +1,12 @@
+// variables 
+let name = "uvesh";
+const Id = 123; 
+var password = 123; // NO 
+city = "Dhrol"; // NO
+
+console.log(name);
+console.log(Id);
+console.log(password);
+console.log(city);
+console.log("hi i am uvesh");
+
